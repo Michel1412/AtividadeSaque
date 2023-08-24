ecadc689f821b9b324fef00843a48ae9881c5914
Michel1412 <michelbocchijr@gmail.com>
2023-08-24 11:03:48 -0300

    removido o espaço

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         ContaBanco c1 = new ContaBanco();
-        c1.abrirConta("teste ","CC", 123456);
+        c1.abrirConta("teste","CC", 123456);
         c1.setSaldo(new BigDecimal("1000"));
         c1.sacarDinheiro("600");
         //System.out.println("Seu novo saldo é " + c1.getSaldo());
